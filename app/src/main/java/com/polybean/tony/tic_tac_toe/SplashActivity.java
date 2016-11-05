fc8e9f2bf2d93185b509e23cc175a2e24750d329
Antony Lulciuc <tonylulciuc@gmail.com>
2016-11-05 11:08:50 -0700

    Added Main Menu Activity Changed Splash theme name

--- app/src/main/java/com/polybean/tony/tic_tac_toe/SplashActivity.java
+++ app/src/main/java/com/polybean/tony/tic_tac_toe/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.polybean.tony.tic_tac_toe;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +11,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        launchActivity();
+    }
 
-
+    /**
+     * Launch MainMenuActivity
+     */
+    private void launchActivity(){
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
