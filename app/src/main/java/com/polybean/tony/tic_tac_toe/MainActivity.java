76a4108d5069438ae230342250e02ba55ac4d694
Antony Lulciuc <tonylulciuc@gmail.com>
2016-11-05 15:12:50 -0700

    Created fragment which hold the board game

--- app/src/main/java/com/polybean/tony/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/polybean/tony/tic_tac_toe/MainActivity.java
@@ -1,5 +1,8 @@
 package com.polybean.tony.tic_tac_toe;
 
+import android.app.FragmentTransaction;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +11,11 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.polybean.tony.tic_tac_toe.board.BoardFragment;
+
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mLeftDrawer;
+    private BoardFragment mBoardFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
 
         // Acquire drawer view
         mLeftDrawer = (DrawerLayout)findViewById(R.id.left_drawer);
+
+        // create board
+        createBoard();
+    }
+
+    /**
+     * Create tic-tac-toe board
+     */
+    private void createBoard(){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment;
+
+        // If fragment already exists
+        if ((fragment = fragmentManager.findFragmentByTag("board")) != null){
+            mBoardFragment = (BoardFragment)fragment;
+            fragmentTransaction.replace(R.id.details_fragment, mBoardFragment, "board");
+        }else{
+            mBoardFragment = new BoardFragment();
+            fragmentTransaction.add(R.id.details_fragment, mBoardFragment, "board");
+        }
+
+        // Commit fragment
+        fragmentTransaction.commit();
     }
 
 
---
+++ app/src/main/java/com/polybean/tony/tic_tac_toe/board/BoardFragment.java
@@ -0,0 +1,29 @@
+package com.polybean.tony.tic_tac_toe.board;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.polybean.tony.tic_tac_toe.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BoardFragment extends Fragment {
+
+
+    public BoardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_board, container, false);
+    }
+}
