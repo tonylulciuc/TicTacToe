76b883b2dbb6adb3f9316bfa0a113080ec8dba02
Antony Lulciuc <tonylulciuc@gmail.com>
2016-11-05 10:21:59 -0700

    Initial commit

---
+++ app/src/main/java/com/polybean/tony/tic_tac_toe/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.polybean.tony.tic_tac_toe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
